6ae1b6f27c4e633b5f6c617ad68e9d816d761519
Jonas Bernardino <jonas.bernardino@dcx.ufpb.br>
2020-12-20 23:12:34 -0300

    Criando operação de instaciação

--- src/main/java/com/jonasbernardino/cursomc/CursomcApplication.java
+++ src/main/java/com/jonasbernardino/cursomc/CursomcApplication.java
@@ -1,13 +1,36 @@
 package com.jonasbernardino.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.jonasbernardino.cursomc.domain.Categoria;
+import com.jonasbernardino.cursomc.repositories.CategoriaRepository;
+
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+		
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		Categoria cat3 = new Categoria(null, "Comida");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
+		
 	}
 
 }
