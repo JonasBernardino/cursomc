f02f2332be858f2a92879c99308da11fbfe64f24
jonasbernardino <you@example.com>
2020-12-29 11:56:58 -0300

    Item pedido e Item pedido PK

--- src/main/java/com/jonasbernardino/cursomc/CursomcApplication.java
+++ src/main/java/com/jonasbernardino/cursomc/CursomcApplication.java
@@ -1,7 +1,6 @@
 package com.jonasbernardino.cursomc;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +13,7 @@ import com.jonasbernardino.cursomc.domain.Cidade;
 import com.jonasbernardino.cursomc.domain.Cliente;
 import com.jonasbernardino.cursomc.domain.Endereco;
 import com.jonasbernardino.cursomc.domain.Estado;
+import com.jonasbernardino.cursomc.domain.ItemPedido;
 import com.jonasbernardino.cursomc.domain.Pagamento;
 import com.jonasbernardino.cursomc.domain.PagamentoComBoleto;
 import com.jonasbernardino.cursomc.domain.PagamentoComCartao;
@@ -26,6 +26,7 @@ import com.jonasbernardino.cursomc.repositories.CidadeRepository;
 import com.jonasbernardino.cursomc.repositories.ClienteRepository;
 import com.jonasbernardino.cursomc.repositories.EnderecoRepository;
 import com.jonasbernardino.cursomc.repositories.EstadoRepository;
+import com.jonasbernardino.cursomc.repositories.ItemPedidoRepository;
 import com.jonasbernardino.cursomc.repositories.PagamentoRepository;
 import com.jonasbernardino.cursomc.repositories.PedidoRepository;
 import com.jonasbernardino.cursomc.repositories.ProdutoRepository;
@@ -56,6 +57,9 @@ public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -123,5 +127,20 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		pedidoRepository.saveAll(Arrays.asList(ped1,ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1,pagto2));
+		
+		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1,ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1,ip2,ip3));
+
 	}
 }
--- src/main/java/com/jonasbernardino/cursomc/domain/Produto.java
+++ src/main/java/com/jonasbernardino/cursomc/domain/Produto.java
@@ -2,7 +2,9 @@ package com.jonasbernardino.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,6 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
 import org.hibernate.annotations.ManyToAny;
 
@@ -34,6 +37,9 @@ public class Produto implements Serializable{
 	)
 	private List<Categoria> categorias = new ArrayList<>();
 	
+	@OneToMany(mappedBy = "id.produto")
+	private Set<ItemPedido> itens = new HashSet<>();
+	
 	public Produto() {
 		
 	}
@@ -44,6 +50,15 @@ public class Produto implements Serializable{
 		this.nome = nome;
 		this.preço = preço;
 	}
+	
+	public List<Pedido> getPedido(){
+		List<Pedido> lista = new ArrayList<>();
+		for(ItemPedido x: itens) {
+			lista.add(x.getPedido());
+		}
+		return lista;
+	}
+	
 
 	public Integer getId() {
 		return id;
@@ -76,6 +91,13 @@ public class Produto implements Serializable{
 	public void setCategorias(List<Categoria> categorias) {
 		this.categorias = categorias;
 	}
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
 
 	@Override
 	public int hashCode() {
@@ -101,6 +123,8 @@ public class Produto implements Serializable{
 			return false;
 		return true;
 	}
+
+
 	
 	
 	
