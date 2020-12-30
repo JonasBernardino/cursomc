27f2feca47ba4f269184ee29754065ec26db5b87
Jonas Bernardino <jonas.bernardino@dcx.ufpb.br>
2020-12-29 23:02:46 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/jonasbernardino/cursomc/services/PedidoService.java
@@ -0,0 +1,23 @@
+package com.jonasbernardino.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jonasbernardino.cursomc.domain.Pedido;
+import com.jonasbernardino.cursomc.repositories.PedidoRepository;
+import com.jonasbernardino.cursomc.services.exception.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+
+	public Pedido find(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+}
